a8d94c923bb90fa27fa518177f919aca60fa90a9
NEPDAVE <nepdave1@gmail.com>
2017-02-17 10:21:29 -0800

    program to build that builds a hashtag staircase

---
+++ staircase.py
@@ -0,0 +1,13 @@
+print "How many stairs in the staircase? Type a number and press Enter:"
+n = int(raw_input().strip())
+def space_maker2(n, staircase):
+    space = n - len(staircase)
+    space_string = ""
+    for i in range(space):
+        space_string += " "
+    return space_string
+
+staircase = ""
+for i in range(n):
+    staircase += "#"
+    print space_maker2(n, staircase) + staircase
